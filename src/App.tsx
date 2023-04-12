4b79fbcea35f16a66ec9875d95ec1c3a704c6216
Cristian Perez <cristian.ap84@gmail.com>
2023-04-12 17:01:54 -0300

    Added basic select component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+
+import styles from './App.module.scss'
+
+import Select from './components/select/Select'
+import { Option } from './types/types'
 
 type Component = () => JSX.Element
 
 const App: Component = () => {
+  const OPTION = { value: '2', label: 'This one is the second option.' }
+  const OPTIONS = [
+    {
+      value: '1',
+      label: 'Here you can find the first option.',
+    },
+    OPTION,
+    { value: '3', label: 'Last but not least, you have here the third option.' },
+  ]
+
+  const handleInputChange = (value: string) => {
+    console.log('Input change')
+  }
+
+  const handleSelection = (option: Option) => {
+    console.log('Option selected')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.example}>
+        <Select options={OPTIONS} isReadOnly={false} onChange={handleInputChange} onSelect={handleSelection} />
+      </div>
     </div>
   )
 }
